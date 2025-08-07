410c752974440b20bff84d4cf79bbf75294c29d0
r258369 <ratulhalderjsr007@gmail.com>
2025-08-07 19:38:46 +0600

    Update firebase_config.py

--- firebase_config.py
+++ firebase_config.py
@@ -2,7 +2,7 @@
 import firebase_admin
 from firebase_admin import credentials, firestore
 
-cred = credentials.Certificate("elearning-8634a-firebase-adminsdk-fbsvc-54472e98d2.json")
+cred = credentials.Certificate("FIREBASE_ADMIN_CREDENTIALS")
 
 # Initialize Firebase with storage bucket configuration
 firebase_admin.initialize_app(cred, {
