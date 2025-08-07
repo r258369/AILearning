a6b3809e3c40acab92a0a52f9f1a51f5745d141b
r258369 <ratulhalderjsr007@gmail.com>
2025-08-07 20:18:56 +0600

    Update firebase_config.py

--- firebase_config.py
+++ firebase_config.py
@@ -3,7 +3,7 @@ import json
 from firebase_admin import credentials
 
 # Read the JSON string from environment variable
-firebase_creds_json = os.environ.get("FIREBASE_ADMIN_CREDENTIALS_JSON")  # use your actual env var name here
+firebase_creds_json = os.environ.get("FIREBASE_ADMIN_CREDENTIALS")  # use your actual env var name here
 
 if not firebase_creds_json:
     raise Exception("FIREBASE_ADMIN_CREDENTIALS_JSON environment variable not set.")
